a5fc4b5b75333fabe8c613a81da0e212802f6e32
Daniel Juventude <deljuven@ymail.com>
2024-05-30 16:14:22 -0300

    pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, PaginaInicialComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cg_manager';
